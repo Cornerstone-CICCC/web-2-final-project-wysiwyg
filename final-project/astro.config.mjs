5dbf893d3247f920532955c4ebdb4e26a7f3d76f
Ikhlak <ikhlak789@gmail.com>
2025-01-09 02:05:05 -0800

    [FEAT] Added Testimonials and Sliders

--- final-project/astro.config.mjs
+++ final-project/astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [legacy()],
+  },
+});
